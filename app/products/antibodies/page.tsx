9e0ce2a343f198a1a5c0c09763139a90c7c266b9
Conner <dreamframe.dev@gmail.com>
2026-01-18 12:35:26 -0800

    Add PBL's MAbs description paragraph to antibodies page

--- app/products/antibodies/page.tsx
+++ app/products/antibodies/page.tsx
@@ -52,6 +52,9 @@ export default function AntibodiesPage() {
           <p>
             Monoclonal antibodies (MAbs) are monospecific antibodies that are the same because they are made by identical immune cells that are all clones of a unique parent cell. MAbs have monovalent affinity, in that they bind to the same epitope and offer high specificity in applications such as ELISA, flow cytometry or epitope mapping where specific binding is desirable.
           </p>
+          <p>
+            PBL&apos;s MAbs are derived against a range of interferons from various mammalian species. They deliver superior product stability and high specificity while providing reliable and reproducible results.
+          </p>
         </div>
       </section>
 
